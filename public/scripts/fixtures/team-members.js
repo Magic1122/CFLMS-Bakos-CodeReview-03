6d9578b5cdc8412df91700849b644d438183cec9
Balint Bakos <balint.bb@gmail.com>
2020-05-29 19:56:01 +0200

    fixes picture path on teams page

--- public/scripts/fixtures/team-members.js
+++ public/scripts/fixtures/team-members.js
@@ -3,15 +3,15 @@ const teamMembers = [
         name:"Fran", 
         position: "CEO", 
         email:"fran@index.com", 
-        image:"/public/img/cat1.jpg"
+        image:"img/cat1.jpg"
     }, 
     {
         name:"Bill", 
         position: 'Front-end Dev',
         email:"bill@index.com", 
-        image:"/public/img/cat2.jpg"}, 
+        image:"img/cat2.jpg"}, 
     {
         name:"Fritz", 
         position: 'Back-end Dev', 
         email:"fritz@index.com", 
-        image:"/public/img/cat3.jpg"}];
+        image:"img/cat3.jpg"}];
